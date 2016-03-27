2723e185259cf9e931516ae627d04035b99b03b5
pakorn0101 <pakorn0101@gmail.com>
2016-03-27 13:57:06 +0700

    Update config.js

--- server/config.js
+++ server/config.js
@@ -1,5 +1,5 @@
 const config = {
-  mongoURL: process.env.MONGO_URL || 'mongodb://52.37.144.132:27017/mern-starter',
+  mongoURL: process.env.MONGO_URL || 'mongodb://52.34.26.58:27017/mern-starter',
   port: process.env.PORT || 8000,
 };
 
